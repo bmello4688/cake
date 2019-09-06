a5dbdd6fcea70178a6182589bbb2000c0b62f55f
Brian Mello <brian.mello@outlook.com>
2019-09-06 14:47:38 -0400

    Added ability to use preview version
    Using 2019 Preview because I want to use .net core 3.0

--- src/Cake.Common/Tools/MSBuild/MSBuildResolver.cs
+++ src/Cake.Common/Tools/MSBuild/MSBuildResolver.cs
@@ -129,7 +129,8 @@ namespace Cake.Common.Tools.MSBuild
                 "Enterprise",
                 "Professional",
                 "Community",
-                "BuildTools"
+                "BuildTools",
+                "Preview"
             };
 
             var visualStudio2017Path = environment.GetSpecialPath(SpecialPath.ProgramFilesX86);
